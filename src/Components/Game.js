642431229dbd15f3e333fe505d322aee8aab297b
Marcus Crane <marcus@thingsima.de>
2017-03-25 06:16:39 +1300

    Added some css tweaks

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import './Game.css'
 
 const Game = (props) => {
     return (
-        <div className="column is-narrow">
+        <div className="column is-narrow is-mobile">
             <div className="notification is-dark">
                 <h5 className="title is-5">{props.title}</h5>
                 <h6 className="subtitle is-6">{props.releaseDate}</h6>
-                <span className="tag">PS4</span>
-                <span className="tag">Xbox One</span>
+                <div className="platforms">
+                    <span className="tag">PS4</span>
+                    <span className="tag">Xbox One</span>
+                </div>
             </div>
         </div>
     )
