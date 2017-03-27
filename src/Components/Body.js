02c4dbf0810381f90e8fd2ec5a21a435dda0bfeb
Marcus Crane <marcus@thingsima.de>
2017-03-27 20:24:03 +1300

    Recentered text

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,14 +3,14 @@ import Game from './Game.js'
 
 class Body extends Component {
     render() {
-        const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', image: '', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', image: '', platforms: ['PS4', 'Switch'] }]
+        const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', image: 'https://imgkk.com/i/plx5.jpg', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', image: 'https://imgkk.com/i/ge5e.jpg', platforms: ['PS4'] }]
         return (
             <section className="section">
                 <div className="container">
                     <h1 className="title">January</h1>
                     <hr />
                     <div className="columns is-desktop is-multiline">
-                        {games.map((game, index) => <Game key={index} title={game.title} releaseDate={game.releaseDate} platforms={game.platforms} /> )}
+                        {games.map((game, index) => <Game key={index} title={game.title} releaseDate={game.releaseDate} image={game.image} platforms={game.platforms} /> )}
                     </div>
                 </div>
             </section>
