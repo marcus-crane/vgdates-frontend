5fd4c72ae07918e87723e6991309ab3c0cf301e5
Marcus Crane <marcus@thingsima.de>
2017-03-27 20:15:32 +1300

    Fixed key requirements for iterators/list

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,14 +3,14 @@ import Game from './Game.js'
 
 class Body extends Component {
     render() {
-        const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', platforms: ['PS4', 'Switch'] }]
+        const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', image: '', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', image: '', platforms: ['PS4', 'Switch'] }]
         return (
             <section className="section">
                 <div className="container">
                     <h1 className="title">January</h1>
                     <hr />
                     <div className="columns is-desktop is-multiline">
-                        {games.map((game) => <Game title={game.title} releaseDate={game.releaseDate} platforms={game.platforms} /> )}
+                        {games.map((game, index) => <Game key={index} title={game.title} releaseDate={game.releaseDate} platforms={game.platforms} /> )}
                     </div>
                 </div>
             </section>
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,7 +8,7 @@ const Game = (props) => {
                 <h5 className="title is-5">{props.title}</h5>
                 <h6 className="subtitle is-6">{props.releaseDate}</h6>
                 <div className="platforms has-text-centered">
-                    {props.platforms.map((platform) => <span className="tag" key={props.title + '-' + platform}>{platform}</span> )}
+                    {props.platforms.map((platform, index) => <span className="tag" key={platform}>{platform}</span> )}
                 </div>
             </div>
         </div>
