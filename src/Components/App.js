616235374897f765c75f2d01c7f26cb3693461e2
Marcus Crane <marcus@thingsima.de>
2017-03-30 13:34:17 +1300

    Converted App and Body to stateless functional components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from './Layout'
 import Body from './Body'
 
-class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <Body />
-      </Layout>
-    );
-  }
+const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', image: 'https://imgkk.com/i/plx5.jpg', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', image: 'https://imgkk.com/i/ge5e.jpg', platforms: ['PS4'] }]
+
+const App = () => {
+  return (
+    <Layout>
+      <Body games={games} />
+    </Layout>
+  );
 }
 
 export default App;
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Month from './Month'
 import Game from './Game'
 
-class Body extends Component {
-  render() {
-    const games = [{ title: 'Gravity Rush', releaseDate: 'January 18', image: 'https://imgkk.com/i/plx5.jpg', platforms: ['PS4'] }, {title: 'Yakuza 0', releaseDate: 'January 24', image: 'https://imgkk.com/i/ge5e.jpg', platforms: ['PS4'] }]
-    return (
-      <Month month="January">
-        {games.map((game, index) => <Game key={index} title={game.title} releaseDate={game.releaseDate} image={game.image} platforms={game.platforms} /> )}
-      </Month>
-    )
-  }
+const Body = (props) => {
+  return (
+    <Month month="January">
+      {props.games.map((game, index) => <Game key={index} title={game.title} releaseDate={game.releaseDate} image={game.image} platforms={game.platforms} /> )}
+    </Month>
+  )
 }
 
 export default Body
